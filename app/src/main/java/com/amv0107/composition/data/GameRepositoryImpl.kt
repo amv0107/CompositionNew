66fbdd0cc276ddb705ee0b99b7e2deed85360c24
amv0107 <amv0107@gmail.com>
2023-02-25 21:17:20 +0200

    Create data layer

---
+++ app/src/main/java/com/amv0107/composition/data/GameRepositoryImpl.kt
@@ -0,0 +1,66 @@
+package com.amv0107.composition.data
+
+import com.amv0107.composition.domian.entity.GameSettings
+import com.amv0107.composition.domian.entity.Level
+import com.amv0107.composition.domian.entity.Question
+import com.amv0107.composition.domian.repository.GameRepository
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.random.Random
+
+object GameRepositoryImpl : GameRepository {
+
+    private const val MIN_SUM_VALUE = 2
+    private const val MIN_ANSWER_VALUE = 1
+
+    override fun generateQuestion(maxSumValue: Int, countOfOptions: Int): Question {
+        val sum = Random.nextInt(MIN_SUM_VALUE, maxSumValue + 1)
+        val visibleNumber = Random.nextInt(MIN_ANSWER_VALUE, sum)
+        val options = HashSet<Int>()
+        val rightAnswer = sum - visibleNumber
+        options.add(rightAnswer)
+        val from = max(rightAnswer - countOfOptions, MIN_ANSWER_VALUE)
+        val to = min(maxSumValue, rightAnswer + countOfOptions)
+        while (options.size < countOfOptions) {
+            Random.nextInt(from, to)
+        }
+        return Question(sum, visibleNumber, options.toList())
+    }
+
+    override fun getGameSettings(level: Level): GameSettings {
+        return when (level) {
+            Level.TEST -> {
+                GameSettings(
+                    10,
+                    3,
+                    50,
+                    8
+                )
+            }
+            Level.EASY -> {
+                GameSettings(
+                    10,
+                    10,
+                    70,
+                    60
+                )
+            }
+            Level.NORMAL -> {
+                GameSettings(
+                    20,
+                    20,
+                    80,
+                    40
+                )
+            }
+            Level.HARD -> {
+                GameSettings(
+                    30,
+                    30,
+                    90,
+                    40
+                )
+            }
+        }
+    }
+}
